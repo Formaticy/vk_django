b8ebf6e4cd2f61dd4f3b0cb841bed3d2498c59a4
Михаил Калинин <m2112623@edu.misis.ru>
2023-05-08 17:13:21 +0300

    add models (users, friends, statuses)

--- DjangoAPI/SocialNetworkApp/models.py
+++ DjangoAPI/SocialNetworkApp/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Test(models.Model):
-    TestId = models.AutoField(primary_key=True)
-    TestName = models.CharField(max_length=400)
+class Users(models.Model):
+    userId = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+
+
+class Statuses(models.Model):
+    statusId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+
+class Friends(models.Model):
+    friendId = models.AutoField(primary_key=True)
+    friend_one = models.ForeignKey(Users, on_delete=models.CASCADE)
+    friend_two = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='friend_set')
+    statusId = models.ForeignKey(Statuses, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        unique_together = (('friend_one', 'friend_two'),)
