58fc293e112c1c1273f4154d9f93b6b65b538494
Михаил Калинин <m2112623@edu.misis.ru>
2023-05-10 00:11:42 +0300

    added path for openAPI (Swagger)

--- DjangoAPI/DjangoAPI/urls.py
+++ DjangoAPI/DjangoAPI/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('SocialNetworkApp.urls'))
+    path('', include('SocialNetworkApp.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger')
 ]
