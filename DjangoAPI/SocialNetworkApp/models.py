23363e2b327d6d222db6d9b3b79b6d4b857a0e42
Михаил Калинин <m2112623@edu.misis.ru>
2023-05-10 00:11:02 +0300

    put unique=True for Statuses.name

--- DjangoAPI/SocialNetworkApp/models.py
+++ DjangoAPI/SocialNetworkApp/models.py
@@ -10,14 +10,14 @@ class Users(models.Model):
 
 class Statuses(models.Model):
     statusId = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
 
 class Friends(models.Model):
     friendId = models.AutoField(primary_key=True)
     friend_one = models.ForeignKey(Users, on_delete=models.CASCADE)
     friend_two = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='friend_set')
-    statusId = models.ForeignKey(Statuses, on_delete=models.SET_NULL, null=True)
+    statusId = models.ForeignKey(Statuses, on_delete=models.SET_NULL, null=True, default=2)
 
     class Meta:
         unique_together = (('friend_one', 'friend_two'),)
